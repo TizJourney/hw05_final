5eaddd5f2cd1cc895b5144947b26e40e7248a5f0
Konstantin Bondar <kostya.bondar@gmail.com>
2020-08-26 16:00:41 +0300

    feat: установка django debug tool

--- yatube/settings.py
+++ yatube/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -113,3 +115,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 SITE_ID = 1
+
+#django-debug-toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- yatube/urls.py
+++ yatube/urls.py
@@ -39,3 +39,6 @@ if settings.DEBUG:
         settings.STATIC_URL,
         document_root=settings.STATIC_ROOT
     )
+
+    import debug_toolbar
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
